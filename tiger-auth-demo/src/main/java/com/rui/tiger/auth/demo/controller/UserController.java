567307228331eef3ef052976e7e72c3e94f8feee
CaiRui <277152345@qq.com>
2018-12-09 15:40:49 +0800

    获取用户认证信息

--- tiger-auth-demo/src/main/java/com/rui/tiger/auth/demo/controller/UserController.java
+++ tiger-auth-demo/src/main/java/com/rui/tiger/auth/demo/controller/UserController.java
@@ -1,5 +1,9 @@
 package com.rui.tiger.auth.demo.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,4 +21,26 @@ public class UserController {
     public String hello() {
         return "Hello,World";
     }
+
+    /**
+     *获取用户认证信息
+     * @return
+     */
+   @GetMapping("authentication")
+    public Authentication getCurrentAuthentication(){
+      return SecurityContextHolder.getContext().getAuthentication();
+    }
+
+    /**
+     * 获取用户认证信息
+     * 同getCurrentAuthentication spring 会帮我们注入
+     * @param authentication
+     * @return
+     */
+    @GetMapping("authentication/auto")
+    public Authentication getCurrentAuthentication2(Authentication authentication){
+        return authentication;
+    }
+
+
 }
